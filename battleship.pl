d041679238ed369a89f633136cba5fc13e7e4d6c
unknown <Leroy@leroygordo.(none)>
2011-10-30 21:19:28 -0430

    sirve_solo

--- battleship.pl
+++ battleship.pl
@@ -1,4 +1,15 @@
 % -*- Mode: Prolog -*-
+isin(_,[],'f').
+isin(_,[],'g').
+isin(Elem,[L|Ls],S):-
+	isinaux(Elem,L,S),
+	isin(Elem,Ls,S).
+	
+isinaux(pos(X,Y),[],S).
+isinaux(pos(X,Y),[pos(X,Y)|Ls],'g').
+isinaux(pos(X,Y),[L|Ls],S):-
+	isinaux(pos(X,Y),Ls,S).
+
 tableroinicial(0,_,[]).
 tableroinicial(F,C,[L|Ls]):-
 	llenarfila(F,C,L),
@@ -22,8 +33,48 @@ mostrartableroaux([L|Ls]):-
 	write(L),
 	mostrartableroaux(Ls).
 
-colocarBarcos(0).
-colocarBarcos(B):-
+ataquesposibles(F,C,[],[]).
+ataquesposibles(F,C,[[]|Ts],L):-
+       Fnew is F +1,
+       Cnew is 0,
+       ataquesposibles(Fnew,Cnew,Ts,L).
+ataquesposibles(F,C,[['a'|Es]|Ts],[pos(F,C)|Ls]):-
+       Cnew is C +1,
+       ataquesposibles(F,Cnew,[Es|Ts],Ls).
+ataquesposibles(F,C,[[_|Es]|Ts],L):-
+       Cnew is C +1,
+       ataquesposibles(F,Cnew,[Es|Ts],L).
+	
+ataque(_,_,0,0).
+ataque(_,_,_,0).
+ataque(_,_,0,_).
+ataque(T0,T1,F,C):-
+	write('Fila a atacar: '),
+	read(Fa),
+	write('Columna a atacar: '),
+	read(Ca),
+	numbarcos(Num),
+	barcos(L),
+	buscarbarco(Num,Lt,L),
+	isin(pos(Fa,Ca),Lt,Hit),
+	ataqueaux(T0,T1,Fa,Ca,Hit).
+
+ataqueaux([T0|T0s],[T1|T0s],0,C,Hit):-
+	ataqueaux2(T0,T1,C,Hit).
+ataqueaux([T0|T0s],[T0|T1],F,C,Hit):-
+	Fnew is F -1,
+	ataqueaux(T0s,T1,Fnew,C,Hit).
+
+ataqueaux2(['g'|T0s],['g'|T0s],0,_).
+ataqueaux2(['f'|T0s],['f'|T0s],0,_).
+ataqueaux2(['h'|T0s],['h'|T0s],0,_).
+ataqueaux2(['a'|T0s],[Hit|T0s],0,Hit).
+ataqueaux2([T0|T0s],[T0|T1],C,Hit):-
+	Cnew is C -1,
+	ataqueaux2(T0s,T1,Cnew,Hit).
+
+colocarBarcos(0,[]).
+colocarBarcos(B,[L,Ls]):-
 	B > 0,
 	write('Informacion de Barco:'),
 	nl,
@@ -35,31 +86,36 @@ colocarBarcos(B):-
 	read(FilaI),
 	write('Columna Inicial: '),
 	read(ColumnaI),
-	assert(barco(pos(FilaI,ColumnaI),tam(T),dir(D))),
+	L = barco(pos(FilaI,ColumnaI),tam(T),dir(D)),
 	B1 is B - 1,
-	colocarBarcos(B1).
+	colocarBarcos(B1,Ls).
 
-ataque(_,_,0,0).
-ataque(_,_,_,0).
-ataque(_,_,0,_).
-ataque(T0,T1,F,C):-
-       write('Fila a atacar: '),
-       read(Fa),
-       write('Columna a atacar: '),
-       read(Ca),
-       ataqueaux(T0,T1,Fa,Ca).
+buscarbarco(0,[],[]).
+buscarbarco(N,[L|Ls],[barco(pos(FilaI,ColumnaI),tam(T),dir(D))|[Lsb]]):-
+	agregarbarco(FilaI,ColumnaI,T,D,L),
+	NNew is N -1,
+	buscarbarco(NNew,Ls,Lsb).
 
-ataqueaux([T0|T0s],[T1|T0s],0,C):-
-       ataqueaux2(T0,T1,C).
-ataqueaux([T0|T0s],[T0|T1],F,C):-
-       Fnew is F -1,
-       ataqueaux(T0s,T1,Fnew,C).
-
-ataqueaux2(['b'|T0s],['g'|T0s],0).
-ataqueaux2(['a'|T0s],['f'|T0s],0).
-ataqueaux2([T0|T0s],[T0|T1],C):-
-       Cnew is C -1,
-       ataqueaux2(T0s,T1,Cnew).
+agregarbarco(_,_,0,'v',[]).
+agregarbarco(_,_,0,'h',[]).
+agregarbarco(F,C,T,'v',[L|Ls]):-
+	Fnew is F +1,
+	Tnew is T -1,
+	L = pos(F,C),
+	agregarbarco(Fnew,C,Tnew,'v',Ls).
+agregarbarco(F,C,T,'h',[L|Ls]):-
+	Cnew is C +1,
+	Tnew is T -1,
+	L = pos(F,C),
+	agregarbarco(F,Cnew,Tnew,'h',Ls).
+	
+ciclo(_,0,_,_).
+ciclo(T,Num,F,C):-
+	Numnew is Num -1,
+	numbarcos(NumB),
+	ataque(T,Tnew,F,C),
+	mostrartablero(Tnew),
+	ciclo(Tnew,Numnew,F,C).
 	
 jugar:-
 	write('Num. de Filas: '),
@@ -69,10 +125,13 @@ jugar:-
 	write('Cant. de Barcos: '),
 	read(NBarcos),
 	assert(tamano(NFilas,NColumnas)),
-	assert(barcos(NBarcos)),
-	tableroinicial(NFilas,NColumnas,Tdp),
-	%ponerbarcos(NBarcos,Tdp,Tbp),
-	mostrartablero(Tdp),
-	colocarBarcos(NBarcos),
-	%ciclopricipal(),
-	retractall(tamano(X,Y)).
+	assert(numbarcos(NBarcos)),
+	tableroinicial(NFilas,NColumnas,Tp),
+	colocarBarcos(NBarcos,Lb),
+	assert(barcos(Lb)),
+	mostrartablero(Tp),
+	ciclo(Tp,4,NFilas,NColumnas),
+	retractall(tamano(X,Y)),
+	retractall(barcos(P)),
+	retractall(numbarcos(Z)),
+	retractall(barco(W,V,O)).
